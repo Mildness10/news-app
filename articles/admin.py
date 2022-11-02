96a0e5850933f354d33c5cf1c8992860bb3a3db0
Mildness Akomoize <66227187+Mildness10@users.noreply.github.com>
2022-11-01 23:52:39 -0400

    Update admin.py

--- articles/admin.py
+++ articles/admin.py
@@ -9,6 +9,9 @@ class ArticleAdmin(admin.ModelAdmin):
     inlines = [
         CommentInline
     ]
-    
+
+#registering the article model in the admin page
 admin.site.register(Article)
+
+#registering the comment model in the admin page
 admin.site.register(Comment)
