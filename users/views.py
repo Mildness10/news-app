b47124798eff18f4a020a1055a555905e5bf0236
Mildness Akomoize <66227187+Mildness10@users.noreply.github.com>
2023-01-28 23:56:02 -0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
 
-
+# SIGN UP VIEW
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
